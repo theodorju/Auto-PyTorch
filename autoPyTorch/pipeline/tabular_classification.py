6fc6963a866e47bba76fea412910905a38ac5b2b
Ravin Kohli <kohliravin7@gmail.com>
2022-04-07 10:57:53 +0200

    add updates for apt1.0+finetunedpreprocessing

--- autoPyTorch/pipeline/tabular_classification.py
+++ autoPyTorch/pipeline/tabular_classification.py
@@ -282,8 +282,8 @@ class TabularClassificationPipeline(ClassifierMixin, BasePipeline):
 
         steps.extend([
             ("imputer", SimpleImputer(random_state=self.random_state)),
-            # ("variance_threshold", VarianceThreshold(random_state=self.random_state)),
-            # ("coalescer", CoalescerChoice(default_dataset_properties, random_state=self.random_state)),
+            ("variance_threshold", VarianceThreshold(random_state=self.random_state)),
+            ("coalescer", CoalescerChoice(default_dataset_properties, random_state=self.random_state)),
             ("column_splitter", ColumnSplitter(random_state=self.random_state)),
             ("encoder", EncoderChoice(default_dataset_properties, random_state=self.random_state)),
             ("scaler", ScalerChoice(default_dataset_properties, random_state=self.random_state)),
